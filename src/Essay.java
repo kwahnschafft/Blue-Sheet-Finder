66a70332b6c653ab27f3a4b8d0a35b0280b51849
spwing <spwing@mxschool.edu>
2016-05-16 14:45:31 -0400

    Rename BlueSheet to Essay

--- src/Essay.java
+++ src/Essay.java
@@ -1,7 +1,50 @@
-/*
- * Written By: Shannon Wing, Kelly Finke, and Kiara Wahnschafft
- * Date: 5/15/16
- */
-public class Essay {
+import java.util.LinkedList;
 
+public class Essay {
+	private static LinkedList<String> sentences = new LinkedList<String>();
+	
+	/*
+	 * place all sentences of essay into a linked list of sentences (strings)
+	 */
+	public static void parse(String essay) {
+		String temp = "";
+		int i = 0;
+		while(i < essay.length()) {
+			int k = i;
+			while(k < essay.length() && essay.charAt(k) != '.') {
+				temp += essay.charAt(k);
+				k++;
+			}
+			temp += essay.charAt(k);
+			sentences.add(temp);
+			i = k+1;
+			temp = "";
+		}
+	}
+
+	/*
+	 * adds all the words in the sentences to the tree of words
+	 */
+	public static void addWords() {
+		String temp = "";
+		for(String str : sentences) {
+			int i = 0;
+			while(i < str.length()) {
+				int k = i;
+				while(k < str.length() && str.charAt(k) != ' ') {
+					temp += str.charAt(k);
+					k++;
+				}
+				//tree.add(temp, str); //add method of the tree
+				i = k+1;
+				temp = "";
+			}	
+		}
+	}
+	
+	public static void main(String[] args) {
+		parse("Heae ael fwe iajfiawpejf eof. AGwaelkj foiajfewjf ej feljakew;f. alsdfj.");
+		System.out.println(sentences);
+		addWords();
+	}
 }
