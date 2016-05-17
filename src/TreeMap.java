36b3054341940084a2711ab35dcf4a31db650f51
kefinke <kefinke@mxschool.edu>
2016-05-17 09:21:57 -0400

    TreeMap stuff
    Not tested yet, to do on Wednesday

--- src/TreeMap.java
+++ src/TreeMap.java
@@ -6,7 +6,32 @@
  * @version 5/15/16
  *
  */
-public class TreeMap extends java.util.TreeMap<String, WordLoc>{
-
+public class TreeMap extends java.util.TreeMap<String, ListNode2>{
+	//wraps up WordLoc in a ListNode to be stored in the map
+	public ListNode2 put(String key, WordLoc value){
+		ListNode2 node = new ListNode2(value);
+		node.setPrevious(node);
+		node.setNext(node);
+		return put(key, node);
+	}
+	
+	// overrides TreeMap put method
+	// if the key is not already in the tree, adds it,
+	// otherwise, adds the ListNode2 to the linked list attached to existing key
+	public ListNode2 put(String key, ListNode2 value){
+		if(!containsKey(key)){ //if key not aready there, add normally
+			return super.put(key, value);
+		}
+		else{ //key already exists, so add value to the linked list attached to key
+			ListNode2 head = get(key);
+			
+			//add to end of this circular linked list
+			value.setPrevious(head.getPrevious());
+			value.getPrevious().setNext(value);
+			value.setNext(head);
+			head.setPrevious(value);
+			return head;
+		}
+	}
 }
 
