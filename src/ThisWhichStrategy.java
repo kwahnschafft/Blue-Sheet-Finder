bf3ed52a0f608257625d492554fcc6cea8ae7085
spwing <spwing@mxschool.edu>
2016-05-20 13:31:18 -0400

    strategies

--- src/ThisWhichStrategy.java
+++ src/ThisWhichStrategy.java
@@ -6,8 +6,24 @@ public class ThisWhichStrategy implements EssaySearchStrategy {
 
 	@Override
 	public ListNode2[] findInEssay(TreeMap tree) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] searchFor = {"this", "which"};
+			
+			ListNode2[] returning = new ListNode2[2];
+			
+			//search tree for this, then which
+			for (int i = 0; i < searchFor.length; i++)
+			{
+				//when word is found add head of LinkedList to array slot
+				for (String word: tree.keySet())
+				{
+				    if (word.compareTo(searchFor[i]) == 0)
+				    {
+				        returning[i] = tree.get(word);
+				        break;
+				    }
+				}
+			}
+			return returning;
 		
 	}
 
