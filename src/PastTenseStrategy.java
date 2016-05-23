938c6e56f289c56514ced6e7ecd79cb586aef807
kwahnschafft <kwahnschafft@gmail.com>
2016-05-22 20:21:22 -0400

    gui more

--- src/PastTenseStrategy.java
+++ src/PastTenseStrategy.java
@@ -11,9 +11,6 @@
  * Date: 5/15/16
  */
 public class PastTenseStrategy implements DatabaseSearchStrategy{
-
-<<<<<<< HEAD
-=======
 	private static String rule = "<html>" +
 	"<b>I. Use the present tense in writing about a literary work.</b>" +
 	"<ul>" + "<li style='list-style-type: none'><b>(Incorrect)</b> Macbeth <i>hastened</i> home to tell his wife of the king's approach.</li>" 
@@ -22,22 +19,13 @@ public class PastTenseStrategy implements DatabaseSearchStrategy{
 	 + "</ul" + "</html>";
 	
 	@Override
->>>>>>> origin/master
 	public void findInDatabase() {
 		// TODO Auto-generated method stub
 		
 	}
 	
-<<<<<<< HEAD
-	@Override
 	public String getRule() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-=======
-	public static String getRule() {
 		return rule;
 	}
 
->>>>>>> origin/master
 }
