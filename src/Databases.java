db71dbdb7314ea84af1617c83455de5387c960f1
spwing <spwing@mxschool.edu>
2016-05-22 20:01:14 -0400

    database update

--- src/Databases.java
+++ src/Databases.java
@@ -2,16 +2,43 @@
  * Written By: Shannon Wing
  * Date: 5/19/16
  */
+import org.apache.poi.poifs.filesystem.*;
+import org.apache.poi.hwpf.*;
+import org.apache.poi.hwpf.extractor.*;
+import java.io.*;
+
 public class Databases {
 	//hashtable
-	String[] progressiveTense;
-	String[] pastTense;
-	String[] passiveVoice;
+	Hashtable<int, String> progressiveTense;
+	Hashtable<int, String> pastTense;
+	Hashtable<int, String> passiveVoice;
 	
 	//downloads and stores all of the words from a pdf into an array
 	public Databases ()
 	{
-		pastTense = new Past
+		String filesname = "PastTenseVerbs.doc";
+        POIFSFileSystem fs = null;
+        try
+        {
+                  fs = new POIFSFileSystem(new FileInputStream(filesname; 
+                  //Couldn't close the braces at the end as my site did not allow it to close
+
+                  HWPFDocument doc = new HWPFDocument(fs);
+
+          WordExtractor we = new WordExtractor(doc);
+
+          String[] paragraphs = we.getParagraphText();
+
+          System.out.println( "Word Document has " + paragraphs.length + " paragraphs" );
+          for( int i=0; i<paragraphs .length; i++ ) {
+            paragraphs[i] = paragraphs[i].replaceAll("\\cM?\r?\n","");
+                    System.out.println( "Length:"+paragraphs[ i ].length());
+          }
+                }
+                catch(Exception e) { 
+                    e.printStackTrace();
+                }
+         }
 		
 	}
 	
