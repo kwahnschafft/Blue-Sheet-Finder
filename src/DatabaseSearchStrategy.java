10f777a3c5107a4216569e3ecbfe69ea9bcc858c
spwing <spwing@mxschool.edu>
2016-05-27 11:37:06 -0400

    update

--- src/DatabaseSearchStrategy.java
+++ src/DatabaseSearchStrategy.java
@@ -1,5 +1,6 @@
 /*
- * represents a bluesheet search strategy that utilizes databases of hashtables 
+ * an interface that represents bluesheet search strategies 
+ * that utilize databases of hashtables 
  * 
  * Written By: Shannon Wing, Kelly Finke, Kiara Wahnschafft
  * Date: 5/31/16
@@ -8,7 +9,7 @@
 import java.util.ArrayList;
 public interface DatabaseSearchStrategy {
 	
-	//returns an arraylist containing linked lists of WordLocs 
+	//returns an arraylist containing LinkedLists of WordLocs 
 	//that contain the sentences that could maintain the specific bluesheet
 	public ArrayList<ListNode2> findInDatabase(TreeMap tree);
 	
